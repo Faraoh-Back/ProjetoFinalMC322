ba531ab3b03a0c043c4c8eead805d9f6e883b142
Felipe Amaral <95597856+LipeAma@users.noreply.github.com>
2025-11-16 15:48:49 -0300

    Formatting Knight class for improved readability

--- app/src/main/java/org/chess/pieces/Knight.java
+++ app/src/main/java/org/chess/pieces/Knight.java
@@ -1,68 +1,63 @@
 package org.chess.pieces;
 
 import java.util.ArrayList;
-import com.google.common.collect.BiMap;
 
 import org.chess.Color;
 import org.chess.Move;
+import org.chess.Move.MoveType;
 import org.chess.PieceNotInBoard;
 import org.chess.Pos;
-import org.chess.Move.MoveType;
 
+import com.google.common.collect.BiMap;
 
-public class Knight extends NonKing{
+public class Knight extends NonKing {
 
-    public Knight(Color color){
-        super(color);
+  public Knight(Color color) {
+    super(color);
+  }
+
+  public MovesCalcResult calculateMoves(BiMap<Pos, Piece> boardState) throws PieceNotInBoard {
+    // Checks if piece is on the board
+    Pos thisPos = boardState.inverse().get(this);
+    if (thisPos == null) {
+      throw new PieceNotInBoard();
     }
 
-    
+    // This will be the MovesCalcResult atributes
+    ArrayList<Move> validMoves = new ArrayList<Move>();
+    ArrayList<Pos> dependencies = new ArrayList<Pos>();
 
-    public MovesCalcResult calculateMoves(BiMap<Pos, Piece> boardState) throws PieceNotInBoard{
-        //Checks if piece is on the board
-        Pos thisPos = boardState.inverse().get(this);
-        if(thisPos == null){
-            throw new PieceNotInBoard();
+    // getting this piece's position
+    int row = thisPos.row();
+    int column = thisPos.column();
+
+    int[][] possibleMoves = {
+        { row + 3, column + 1 },
+        { row + 3, column - 1 },
+        { row - 3, column + 1 },
+        { row - 3, column - 1 },
+        { row + 1, column + 3 },
+        { row - 1, column + 3 },
+        { row + 1, column - 3 },
+        { row - 1, column - 3 }
+    };
+
+    // creating a list of valid positions in the board
+
+    // Checks if those positions would generate validMoves,
+    // then, fills validMoves and piecesBlockingMoves
+    for (int[] pos : possibleMoves) {
+      try {
+        Pos tempPos = new Pos(pos[0], pos[1]);
+        Piece pieceInPos = boardState.get(tempPos);
+        if (pieceInPos != null && pieceInPos.color == super.color) {
+          dependencies.add(tempPos);
+        } else {
+          validMoves.add(new Move(this, MoveType.SIMPLE_MOVE, tempPos));
         }
-
-        //This will be the MovesCalcResult atributes
-        ArrayList<Move> validMoves = new ArrayList<Move>();
-        ArrayList<Pos> dependencies = new ArrayList<Pos>();
-
-        //getting this piece's position
-        int row = thisPos.row();
-        int column = thisPos.column();
-
-        int[][] possibleMoves = {
-            {row + 3, column + 1},
-            {row + 3, column - 1},
-            {row - 3, column + 1},
-            {row - 3, column - 1},
-            {row + 1, column + 3},
-            {row - 1, column + 3},
-            {row + 1, column - 3},
-            {row - 1, column - 3}
-        };
-
-        //creating a list of valid positions in the board
-
-        //Checks if those positions would generate validMoves,
-        //then, fills validMoves and piecesBlockingMoves
-        for(int[] pos : possibleMoves){
-            try{
-                Pos tempPos = new Pos(pos[0], pos[1]);
-                Piece pieceInPos = boardState.get(tempPos);
-                if(pieceInPos != null && pieceInPos.color == super.color){
-					dependencies.add(tempPos);
-				}else{
-					validMoves.add(new Move(this, MoveType.SIMPLE_MOVE, tempPos));
-				}
-                
-            }catch(IllegalArgumentException e){}
-        }
-
-        return new MovesCalcResult(validMoves, dependencies);
-        
+      } catch (IllegalArgumentException e) {
+      }
     }
-
+    return new MovesCalcResult(validMoves, dependencies);
+  }
 }
