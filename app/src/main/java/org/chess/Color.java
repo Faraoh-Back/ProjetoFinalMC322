f139e419de7df7ebecdd09cd1fb455e7bc80c579
Felipe Amaral <contato.feamaral@gmail.com>
2025-11-20 23:44:35 -0300

    - Refatoração do metodo calculateMoves em métodos menores. - métodos para saber quais cores estão na direita/esquerda/frente forão movidos para a classe Color - Removido constante para saber se peão moveu. Basta consultar se row == 13. - Arrumado bug em que usava (row + 1 == 14) para calcular se o pawn ia ser promovido, mas o certo é (row == 2) - formatação do arquivo

--- app/src/main/java/org/chess/Color.java
+++ app/src/main/java/org/chess/Color.java
@@ -5,4 +5,36 @@ public enum Color {
   RED,
   YELLOW,
   BLUE;
+
+  // returns the color in the front
+  public Color getFrontColor() {
+    return switch (this) {
+      case GREEN -> RED;
+      case YELLOW -> BLUE;
+      case RED -> GREEN;
+      case BLUE -> YELLOW;
+    };
+  }
+
+
+  // returns the color to the left
+  public Color getLeftColor() {
+    return switch (this) {
+      case GREEN -> YELLOW;
+      case YELLOW -> RED;
+      case RED -> BLUE;
+      case BLUE -> GREEN;
+    };
+  }
+
+
+  // returns the color to the right
+  public Color getRightColor() {
+    return switch (this) {
+      case GREEN -> BLUE;
+      case YELLOW -> GREEN;
+      case RED -> YELLOW;
+      case BLUE -> RED;
+    };
+  }
 }
--- app/src/main/java/org/chess/pieces/Pawn.java
+++ app/src/main/java/org/chess/pieces/Pawn.java
@@ -13,179 +13,156 @@ import org.chess.Pos;
 import org.chess.Move.MoveType;
 import org.chess.board.History;
 
+public class Pawn extends Piece {
+  public Pawn(Color color) {
+    super(color);
+  }
 
-public class Pawn extends Piece{
-    boolean thisMovedYet = false;
-    
-    public Pawn(Color color){
-        super(color);
+  public MovesCalcResult calculateMoves(BiMap<Pos, Piece> boardState, History gameHistory) throws PieceNotInBoard {
+
+    // Checks if piece is on the board
+    Pos thisPos = boardState.inverse().get(this);
+    if (thisPos == null) {
+      throw new PieceNotInBoard();
     }
 
-    public MovesCalcResult calculateMoves(BiMap<Pos, Piece> boardState, History gameHistory) throws PieceNotInBoard{
+    // This will be the MovesCalcResult atributes
+    ArrayList<Move> validMoves = new ArrayList<Move>();
+    ArrayList<Pos> dependencies = new ArrayList<Pos>();
 
-        //Checks if piece is on the board
-        Pos thisPos = boardState.inverse().get(this);
-        if(thisPos == null){
-            throw new PieceNotInBoard();
-        }
-        
-        //This will be the MovesCalcResult atributes
-        ArrayList<Move> validMoves = new ArrayList<Move>();
-        ArrayList<Pos> dependencies = new ArrayList<Pos>();
+    // getting this piece's positionu
+    int row = thisPos.row();
+    int column = thisPos.column();
 
-        //getting this piece's position
-        int row = thisPos.row();
-        int column = thisPos.column();
+    // En-passant check
+    enPassantMove(boardState, gameHistory, validMoves, dependencies, row, column);
 
-        //Verifies if this pawn has moved
-        if(!thisMovedYet){
-            List<Move> thisHistory = gameHistory.getMovesView(this);
-            if(!thisHistory.isEmpty()){
-                thisMovedYet = true;
-            }
-        }
+    // Double-step check
+    doubleMove(boardState, validMoves, dependencies, row, column);
 
-        //En-passant check
-        List<Move> LastMoves = gameHistory.getMovesView();
-        Move lastMove = LastMoves.get(LastMoves.size() - 1);
+    // Simple Move check
+    simpleMove(boardState, validMoves, dependencies, row, column);
 
-        //checks if last move was a double pawn move
-        if(lastMove.type() == MoveType.PAWN_DOUBLE){
-            Piece movedPiece = lastMove.piece();
-            Pos tempPos;
+    // Left Attack-Move check
+    leftMove(boardState, validMoves, dependencies, row, column);
 
-            // checks if the moved piece is the color in the front
-            if(getFrontColor(super.color) == movedPiece.color){
-                //checking right-side en-passant
-                tempPos = new Pos(row, column + 1);
-                if(boardState.get(tempPos) == movedPiece){
-                    validMoves.add(new Move(this, MoveType.EN_PASSANT, new Pos(row-1, column+1)));
-                }
+    // Right Attack-Move check
+    rightMove(boardState, validMoves, dependencies, row, column);
 
-                //checking left-side en-passant
-                tempPos = new Pos(row, column - 1);
-                if(boardState.get(tempPos) == movedPiece){
-                    validMoves.add(new Move(this, MoveType.EN_PASSANT, new Pos(row-1, column-1)));
-                }
-            //checking en-passant on the side colors
-            }else{
-                tempPos = new Pos(row - 1, column);
-                if(boardState.get(tempPos) == movedPiece){
-                    if(getLeftColor(super.color) == movedPiece.color){
-                        validMoves.add(new Move(this, MoveType.EN_PASSANT, new Pos(row-1, column-1)));
-                    }else{
-                        validMoves.add(new Move(this, MoveType.EN_PASSANT, new Pos(row-1, column+1)));
-                    }
-                }
-            }
+    return new MovesCalcResult(validMoves, dependencies);
+  }
+
+  private void enPassantMove(BiMap<Pos, Piece> boardState, History gameHistory, ArrayList<Move> validMoves,
+      ArrayList<Pos> dependencies, int row, int column) {
+
+    List<Move> LastMoves = gameHistory.getMovesView();
+    Move lastMove = LastMoves.get(LastMoves.size() - 1);
+
+    // checks if last move was a double pawn move
+    if (lastMove.type() == MoveType.PAWN_DOUBLE) {
+      Piece movedPiece = lastMove.piece();
+      Pos tempPos;
+
+      // checks if the moved piece is the color in the front
+      if (super.color.getFrontColor() == movedPiece.color) {
+        // checking right-side en-passant
+        tempPos = new Pos(row, column + 1);
+        if (boardState.get(tempPos) == movedPiece) {
+          validMoves.add(new Move(this, MoveType.EN_PASSANT, new Pos(row - 1, column + 1)));
         }
 
-        
-        //Double-step check
-        if(!thisMovedYet){
-            Pos movementPos = new Pos(row-2, column);
-            dependencies.add(movementPos);
-            if(boardState.get(movementPos) == null){
-                validMoves.add(new Move(this, MoveType.PAWN_DOUBLE, movementPos));
-            }
+        // checking left-side en-passant
+        tempPos = new Pos(row, column - 1);
+        if (boardState.get(tempPos) == movedPiece) {
+          validMoves.add(new Move(this, MoveType.EN_PASSANT, new Pos(row - 1, column - 1)));
         }
-        
-
-        //Simple Move check
-        Pos movementPos = new Pos(row - 1, column);
-        Piece pieceInPos = boardState.get(movementPos);
-        dependencies.add(movementPos);
-        if(pieceInPos == null){
-            if(row+1 == 14){
-                validMoves.add(new Move(this, MoveType.QUEEN_PROMOTION, movementPos));
-                validMoves.add(new Move(this, MoveType.KNIGHT_PROMOTION, movementPos));
-                validMoves.add(new Move(this, MoveType.ROOK_PROMOTION, movementPos));
-                validMoves.add(new Move(this, MoveType.BISHOP_PROMOTION, movementPos));
-            }else{
-                validMoves.add(new Move(this, MoveType.SIMPLE_MOVE, movementPos));
-            }  
+        // checking en-passant on the side colors
+      } else {
+        tempPos = new Pos(row - 1, column);
+        if (boardState.get(tempPos) == movedPiece) {
+          if (super.color.getLeftColor() == movedPiece.color) {
+            validMoves.add(new Move(this, MoveType.EN_PASSANT, new Pos(row - 1, column - 1)));
+          } else {
+            validMoves.add(new Move(this, MoveType.EN_PASSANT, new Pos(row - 1, column + 1)));
+          }
         }
-
-        //Left Attack-Move check
-        try{
-            movementPos = new Pos(row - 1, column - 1);
-            dependencies.add(movementPos);
-            pieceInPos = boardState.get(movementPos);
-            
-            if(pieceInPos != null && pieceInPos.color != super.color){
-                if(row+1 == 14){
-                validMoves.add(new Move(this, MoveType.QUEEN_PROMOTION, movementPos));
-                validMoves.add(new Move(this, MoveType.KNIGHT_PROMOTION, movementPos));
-                validMoves.add(new Move(this, MoveType.ROOK_PROMOTION, movementPos));
-                validMoves.add(new Move(this, MoveType.BISHOP_PROMOTION, movementPos));
-                }else{
-                    validMoves.add(new Move(this, MoveType.SIMPLE_MOVE, movementPos));
-                }  
-            }
-        }catch(Exception e){}
-
-        //Right Attack-Move check
-        try{
-            movementPos = new Pos(row - 1, column + 1);
-            dependencies.add(movementPos);
-            pieceInPos = boardState.get(movementPos);
-            
-            if(pieceInPos != null && pieceInPos.color != super.color){
-                if(row+1 == 14){
-                validMoves.add(new Move(this, MoveType.QUEEN_PROMOTION, movementPos));
-                validMoves.add(new Move(this, MoveType.KNIGHT_PROMOTION, movementPos));
-                validMoves.add(new Move(this, MoveType.ROOK_PROMOTION, movementPos));
-                validMoves.add(new Move(this, MoveType.BISHOP_PROMOTION, movementPos));
-                }else{
-                    validMoves.add(new Move(this, MoveType.SIMPLE_MOVE, movementPos));
-                }  
-            }
-        }catch(Exception e){}
-
-        //Add final dependencies that will be needed anyways for checking en-passant
-        dependencies.add(new Pos(row, column+1));
-        dependencies.add(new Pos(row, column-1));
-
-        return new MovesCalcResult(validMoves, dependencies);
+      }
     }
+    // Add final dependencies that will be needed anyways for checking en-passant
+    dependencies.add(new Pos(row, column + 1));
+    dependencies.add(new Pos(row, column - 1));
+  }
 
-//returns the color in the front
-private Color getFrontColor(Color thisColor){
-    Color frontColor = Color.RED;
-    switch(thisColor){
-        case Color.GREEN:
-            frontColor = Color.RED;
-            break;
-        case Color.YELLOW:
-            frontColor = Color.BLUE;
-            break;
-        case Color.RED:
-            frontColor = Color.GREEN;
-            break;
-        case Color.BLUE:
-            frontColor = Color.YELLOW;
-            break;
+  private void doubleMove(BiMap<Pos, Piece> boardState, ArrayList<Move> validMoves, ArrayList<Pos> dependencies,
+      int row, int column) {
+    if (row == 13) {
+      Pos movementPos = new Pos(row - 2, column);
+      dependencies.add(movementPos);
+      if (boardState.get(movementPos) == null) {
+        validMoves.add(new Move(this, MoveType.PAWN_DOUBLE, movementPos));
+      }
     }
-    return frontColor;
-}
+  }
 
-//returns the color in the left
-private Color getLeftColor(Color thisColor){
-    Color frontColor = Color.RED;
-    switch(thisColor){
-        case Color.GREEN:
-            frontColor = Color.YELLOW;
-            break;
-        case Color.YELLOW:
-            frontColor = Color.RED;
-            break;
-        case Color.RED:
-            frontColor = Color.BLUE;
-            break;
-        case Color.BLUE:
-            frontColor = Color.GREEN;
-            break;
+  private void rightMove(BiMap<Pos, Piece> boardState, ArrayList<Move> validMoves, ArrayList<Pos> dependencies,
+      int row, int column) {
+    try {
+      Pos movementPos = new Pos(row - 1, column + 1);
+      dependencies.add(movementPos);
+      Piece pieceInPos = boardState.get(movementPos);
+
+      if (pieceInPos != null && pieceInPos.color != super.color) {
+        if (row == 2) {
+          validMoves.add(new Move(this, MoveType.QUEEN_PROMOTION, movementPos));
+          validMoves.add(new Move(this, MoveType.KNIGHT_PROMOTION, movementPos));
+          validMoves.add(new Move(this, MoveType.ROOK_PROMOTION, movementPos));
+          validMoves.add(new Move(this, MoveType.BISHOP_PROMOTION, movementPos));
+        } else {
+          validMoves.add(new Move(this, MoveType.SIMPLE_MOVE, movementPos));
+        }
+      }
+    } catch (IllegalArgumentException e) {
     }
-    return frontColor;
-}
+  }
+
+  private void leftMove(BiMap<Pos, Piece> boardState, ArrayList<Move> validMoves, ArrayList<Pos> dependencies,
+      int row, int column) {
+    try {
+      Pos movementPos = new Pos(row - 1, column - 1);
+      dependencies.add(movementPos);
+      Piece pieceInPos = boardState.get(movementPos);
+
+      if (pieceInPos != null && pieceInPos.color != super.color) {
+        if (row == 2) {
+          validMoves.add(new Move(this, MoveType.QUEEN_PROMOTION, movementPos));
+          validMoves.add(new Move(this, MoveType.KNIGHT_PROMOTION, movementPos));
+          validMoves.add(new Move(this, MoveType.ROOK_PROMOTION, movementPos));
+          validMoves.add(new Move(this, MoveType.BISHOP_PROMOTION, movementPos));
+        } else {
+          validMoves.add(new Move(this, MoveType.SIMPLE_MOVE, movementPos));
+        }
+      }
+    } catch (IllegalArgumentException e) {
+    }
+  }
+
+  private void simpleMove(BiMap<Pos, Piece> boardState, ArrayList<Move> validMoves, ArrayList<Pos> dependencies,
+      int row, int column) {
+    try {
+      Pos movementPos = new Pos(row - 1, column);
+      Piece pieceInPos = boardState.get(movementPos);
+      dependencies.add(movementPos);
+      if (pieceInPos == null) {
+        if (row == 2) {
+          validMoves.add(new Move(this, MoveType.QUEEN_PROMOTION, movementPos));
+          validMoves.add(new Move(this, MoveType.KNIGHT_PROMOTION, movementPos));
+          validMoves.add(new Move(this, MoveType.ROOK_PROMOTION, movementPos));
+          validMoves.add(new Move(this, MoveType.BISHOP_PROMOTION, movementPos));
+        } else {
+          validMoves.add(new Move(this, MoveType.SIMPLE_MOVE, movementPos));
+        }
+      }
+    } catch (IllegalArgumentException e) {
+    }
+  }
 }
