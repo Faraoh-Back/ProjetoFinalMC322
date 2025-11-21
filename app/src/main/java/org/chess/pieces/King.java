d7257ca512d236d73d6464e5dba6156983cd0293
Rafael Henrique de Gaspi <degaspirafael@gmail.com>
2025-11-20 22:08:23 -0300

    calculateMoves() de King estruturada

--- app/src/main/java/org/chess/pieces/King.java
+++ app/src/main/java/org/chess/pieces/King.java
@@ -1,80 +1,133 @@
 package org.chess.pieces;
 
-import java.security.InvalidKeyException;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 import java.util.function.Predicate;
 import java.util.Map;
+import java.util.List;
 
 import com.google.common.collect.BiMap;
 
 import org.chess.Color;
-import org.chess.board.Board;
+import org.chess.board.History;
 import org.chess.Pos;
 import org.chess.Move.MoveType;
 import org.chess.PieceNotInBoard;
 import org.chess.Move;
 
 public class King extends Piece {
-	private boolean castlingEnable = true;
+	public boolean castlingEnable = true;
+	public boolean inCheck = false;
+	public Piece kingSideRook;
+	public Piece queenSideRook;
 	
 
 	public King(Color color) {
 		super(color);
 	}
 
-	public static MovesCalcResult calculateMoves(Collection<King> kings, BiMap<Pos, Piece> boardState, Map<Color, Predicate<Pos>> dangerMap) throws PieceNotInBoard{
-		Piece kingSideRook;
-		Piece queenSideRook;
+	public static MovesCalcResult calculateMoves(Collection<King> kings, BiMap<Pos, Piece> boardState, Map<Color, Predicate<Pos>> dangerMap, History gameHistory) throws PieceNotInBoard{
+		ArrayList<Move> validMoves = new ArrayList<Move>();
+		ArrayList<Pos> dependencies = new ArrayList<Pos>();
+		
+		Pos thisPos;
 		int row;
 		int column;
-		int[] directionHelper;
 
-		//Normal King Move
-		int[][] possibleMoves = {
-			{row+1, column},
-			{row+1, column+1},
-			{row, column+1},
-			{row-1, column+1},
-			{row-1, column},
-			{row-1, column-1},
-			{row, column-1},
-			{row+1, column-1}
-		};
-		for(int[] pos : possibleMoves){
-			try{
-				Pos tempPos = new Pos(pos[0], pos[1]);
-				Piece pieceInPos = boardState.getPiece(tempPos);
-				if(pieceInPos != null){
-					if(pieceInPos.color == .color){
-						piecesBlockingMoves.add(pieceInPos);
-					}else{
-						checkForDanger(MoveType.SIMPLE_MOVE, tempPos);
+		
+		for(King king : kings){
+			thisPos = boardState.inverse().get(king);
+			row = thisPos.row();
+			column = thisPos.column();
+
+			//Verify if king is in check
+			//TODO: verificar se esta verificação está de acordo coma lógica do programa
+			if(dangerMap.get(king.color).test(thisPos)){
+				king.inCheck = true;
+			}else{
+				king.inCheck = false;
+			}
+
+			//Normal King Move
+			int[][] possibleMoves = {
+				{row+1, column},
+				{row+1, column+1},
+				{row, column+1},
+				{row-1, column+1},
+				{row-1, column},
+				{row-1, column-1},
+				{row, column-1},
+				{row+1, column-1}
+			};
+
+			for(int[] pos : possibleMoves){
+				try{
+					Pos movementPos = new Pos(pos[0], pos[1]);
+					dependencies.add(movementPos);
+					Piece pieceInPos = boardState.get(movementPos);
+					if(pieceInPos == null || pieceInPos.color != king.color){
+						//TODO: verificar se esta verificação está de acordo coma lógica do programa
+						if(dangerMap.get(king.color).test(movementPos)){ 
+							//Entraria aqui se a posição de movimento fosse perigosa
+							//TODO: achar um jeito de colocar a posição da peça que ameaça esta posição em dependencies
+						}else{
+							validMoves.add(new Move(king, MoveType.SIMPLE_MOVE, movementPos));
+						}
+					}
+				}catch(Exception e){}
+			}
+
+			//Castling
+			if(king.castlingEnable && !king.inCheck){
+				List<Move> kingMoves = gameHistory.getMovesView(king);
+				//verifies if king moved
+				if(kingMoves.isEmpty()){
+					List<Move> kingSideMoves = gameHistory.getMovesView(king.kingSideRook);
+					List<Move> queenSideMoves = gameHistory.getMovesView(king.queenSideRook);
+					//verifies if both rooks moved
+					if(kingSideMoves.isEmpty() && queenSideMoves.isEmpty()){
+						king.castlingEnable = false;
+						continue;
+					}
+					//TODO: improve castling verification implementation and find a way to add the positions of danger to dependencies
+					if(!kingSideMoves.isEmpty()){
+						//verifies poorly if there is no pieces between king and rook and if there is danger
+						Pos firstSquare = new Pos(row, column+1);
+						Pos secondSquare = new Pos(row, column+2);
+						if(boardState.get(firstSquare) == null && boardState.get(secondSquare) == null){
+							if(!dangerMap.get(king.color).test(firstSquare) && !dangerMap.get(king.color).test(secondSquare)){
+								validMoves.add(new Move(king, MoveType.KINGSIDE_CASTLING, secondSquare));
+							}
+						}
+					}
+					if(!queenSideMoves.isEmpty()){
+						//verifies poorly if there is no pieces between king and rook and if there is danger
+						Pos firstSquare = new Pos(row, column-1);
+						Pos secondSquare = new Pos(row, column-2);
+						Pos thirdSquare = new Pos(row, column -3);
+						if(boardState.get(firstSquare) == null && boardState.get(secondSquare) == null && boardState.get(thirdSquare) == null){
+							if(!dangerMap.get(king.color).test(firstSquare) && !dangerMap.get(king.color).test(secondSquare) && !dangerMap.get(king.color).test(thirdSquare)){
+								validMoves.add(new Move(king, MoveType.QUEENSIDE_CASTLING, secondSquare));
+							}
+						}
 					}
 				}else{
-					checkForDanger(MoveType.SIMPLE_MOVE, tempPos);
+					king.castlingEnable = false;
 				}
-				
-			}catch(Exception e){}
+			}
+
 		}
 
-
-		return new MovesCalcResult(validMoves, piecesBlockingMoves);
-	}
-	/**
-	 * Verifies if the king would be in danger if moved
-	 * to a position, if not, add valid move to that position,
-	 * if yes, adds pieces that put king in danger to piecesBlockingMove
-	 * @param type
-	 * @param pos
-	 */
-	private void checkForDanger(MoveType type, Pos pos){
 		
+		
+
+
+		return new MovesCalcResult(validMoves, dependencies);
 	}
 
-	private boolean checkCastling(){
-
+	//Atributes to King his rooks, this facilitates castling implementation
+	public void setKingRooks(Rook kingSide, Rook queenSide){
+		this.kingSideRook = kingSide;
+		this.queenSideRook = queenSide;
 	}
-
 }
