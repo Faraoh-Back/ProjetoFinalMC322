082598b8c96305be7608646fa217327f2bf68b5a
LipeAma <contato.feamaral@gmail.com>
2025-11-25 03:17:43 -0300

    Bugs no roque corrigidos

--- app/src/main/java/org/chess/pieces/King.java
+++ app/src/main/java/org/chess/pieces/King.java
@@ -10,6 +10,7 @@ import java.util.function.Predicate;
 import org.chess.Color;
 import org.chess.Move;
 import org.chess.Move.MoveType;
+import org.chess.PieceType;
 import org.chess.exception.InvalidPosition;
 import org.chess.exception.PieceNotInBoard;
 import org.chess.Pos;
@@ -27,7 +28,8 @@ public class King extends Piece {
         this.queenSideRook = queensideRook;
     }
 
-    public static Collection<Move> calculateMoves(Collection<King> kings, Function<Pos, Piece> getPiece, Function<Piece, Pos> getPos,
+    public static Collection<Move> calculateMoves(Collection<King> kings, Function<Pos, Piece> getPiece,
+            Function<Piece, Pos> getPos,
             Function<Color, Predicate<Pos>> getDangerMap, Predicate<Piece> movedBefore) throws PieceNotInBoard {
         Multimap<Pos, King> simpleMoves = HashMultimap.create();
         for (King king : kings) {
@@ -40,42 +42,46 @@ public class King extends Piece {
         validSimpleMoves.forEach((pos, king) -> moves.add(new Move(king, MoveType.SIMPLE_MOVE, pos)));
         for (King king : kings) {
             Predicate<Pos> dangerMap = getDangerMap.apply(king.color);
-            Predicate<Pos> patchedDangerMap = pos -> dangerMap.test(pos) || validSimpleMoves.get(pos) != king;
+            Predicate<Pos> patchedDangerMap = pos -> {
+                var kingAttaking = validSimpleMoves.get(pos);
+                if (dangerMap.test(pos) || (kingAttaking != null && kingAttaking != king))
+                    return true;
+                return false;
+            };
             moves.addAll(king.getCastlingMoves(getPiece, getPos, patchedDangerMap, movedBefore));
         }
         return moves;
     }
 
-    private Collection<Pos> getSimpleMoves(Function<Pos, Piece> getPiece, Function<Piece, Pos> getPos, Predicate<Pos> dangerMap) throws PieceNotInBoard {
-            Pos thisPos = getPos.apply(this);
-            if (thisPos == null)
-                throw new PieceNotInBoard();
-            int row = thisPos.row();
-            int column = thisPos.column();
+    private Collection<Pos> getSimpleMoves(Function<Pos, Piece> getPiece, Function<Piece, Pos> getPos,
+            Predicate<Pos> dangerMap) throws PieceNotInBoard {
+        Pos thisPos = getPos.apply(this);
+        if (thisPos == null)
+            throw new PieceNotInBoard();
+        int row = thisPos.row();
+        int column = thisPos.column();
 
-            // Normal King Move
-            int[][] possibleMoves = { 
-                { row + 1, column }, 
-                { row + 1, column + 1 }, 
+        // Normal King Move
+        int[][] possibleMoves = {
+                { row + 1, column },
+                { row + 1, column + 1 },
                 { row, column + 1 },
                 { row - 1, column + 1 },
                 { row - 1, column },
                 { row - 1, column - 1 },
                 { row, column - 1 },
                 { row + 1, column - 1 }
-            };
-            Collection<Pos> simpleMoves = new ArrayList<>();
-            for (int[] pos : possibleMoves) {
-                try {
-                    Pos movementPos = new Pos(pos[0], pos[1]);
-                    Piece pieceInPos = getPiece.apply(movementPos);
-                    if ((pieceInPos == null || pieceInPos.color != color)
-                            && !dangerMap.test(movementPos)) {
-                        simpleMoves.add(movementPos);
-                    }
-                } catch (InvalidPosition e) {
-                }
+        };
+        Collection<Pos> simpleMoves = new ArrayList<>();
+        for (int[] pos : possibleMoves) {
+            try {
+                Pos movementPos = new Pos(pos[0], pos[1]);
+                Piece pieceInPos = getPiece.apply(movementPos);
+                if ((pieceInPos == null || pieceInPos.color != color) && !dangerMap.test(movementPos))
+                    simpleMoves.add(movementPos);
+            } catch (InvalidPosition e) {
             }
+        }
         return simpleMoves;
     }
 
@@ -92,9 +98,10 @@ public class King extends Piece {
         return moves;
     }
 
-    private Collection<Move> getCastlingMoves(Function<Pos, Piece> getPiece, Function<Piece, Pos> getPos, Predicate<Pos> dangerMap,
+    private Collection<Move> getCastlingMoves(Function<Pos, Piece> getPiece, Function<Piece, Pos> getPos,
+            Predicate<Pos> dangerMap,
             Predicate<Piece> movedBefore) throws PieceNotInBoard {
-        
+
         Collection<Move> moves = new ArrayList<>();
 
         if (movedBefore.test(this))
@@ -109,33 +116,39 @@ public class King extends Piece {
         if (dangerMap.test(thisPos))
             return moves;
 
-        if (!movedBefore.test(kingSideRook))
-            moves.add(kingsideCaslte(row, column, dangerMap, getPiece));
+        if (!movedBefore.test(kingSideRook)) {
+            var move = kingsideCaslte(row, column, dangerMap, getPiece);
+            if (move != null)
+                moves.add(move);
+        }
 
-        if (!movedBefore.test(queenSideRook))
-            moves.add(queensideCaslte(row, column, dangerMap, getPiece));
+        if (!movedBefore.test(queenSideRook)) {
+            var move = queensideCaslte(row, column, dangerMap, getPiece);
+            if (move != null)
+                moves.add(move);
+        }
 
         return moves;
     }
 
     private Move kingsideCaslte(int row, int col, Predicate<Pos> dangerMap, Function<Pos, Piece> getPiece) {
-        int i = color.queenToTheLeftOfKing ? 1 : -1;
-        Pos firstSquare = new Pos(row, col + i);
-        Pos secondSquare = new Pos(row, col + 2 * i);
+        Pos firstSquare = PieceType.KINGSIDE_BISHOP.initialPos(color);
+        Pos secondSquare = PieceType.KINGSIDE_KNIGHT.initialPos(color);
         if (getPiece.apply(firstSquare) != null || getPiece.apply(secondSquare) != null || dangerMap.test(firstSquare)
-                || dangerMap.test(secondSquare))
+                || dangerMap.test(secondSquare)) {
             return null;
+        }
         return new Move(this, MoveType.KINGSIDE_CASTLING, secondSquare);
     }
 
     private Move queensideCaslte(int row, int col, Predicate<Pos> dangerMap, Function<Pos, Piece> getPiece) {
-        int i = color.queenToTheLeftOfKing ? -1 : 1;
-        Pos firstSquare = new Pos(row, col + i);
-        Pos secondSquare = new Pos(row, col + 2 * i);
-        Pos thirdSquare = new Pos(row, col + 3 * i);
+        Pos firstSquare = PieceType.QUEEN.initialPos(color);
+        Pos secondSquare = PieceType.QUEENSIDE_BISHOP.initialPos(color);
+        Pos thirdSquare = PieceType.QUEENSIDE_KNIGHT.initialPos(color);
         if (getPiece.apply(firstSquare) != null || getPiece.apply(secondSquare) != null
-                || getPiece.apply(thirdSquare) != null || dangerMap.test(firstSquare) || dangerMap.test(secondSquare))
+                || getPiece.apply(thirdSquare) != null || dangerMap.test(firstSquare) || dangerMap.test(secondSquare)) {
             return null;
+        }
         return new Move(this, MoveType.QUEENSIDE_CASTLING, secondSquare);
     }
 }
