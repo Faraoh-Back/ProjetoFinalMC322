c32ab2ca00defd5de95651466842c87fce5ab75a
Marcos <marcos.santos.peresvw@gmail.com>
2025-11-30 14:52:07 -0300

    Gerenciamento de persistencia

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
     implementation("com.fasterxml.jackson.core:jackson-databind:2.15.3")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.google.code.gson:gson:2.10.1")  
 }
 
 java {
